c2b5003ebb058849bfae23e623d1ddce5bc14397
Richard-VisualizeMyCode <richard@visualizemycode.com>
2024-12-18 00:18:34 -0500

    Frontend (#16)
    * Initialize project using Create React App
    
    * Startup code for the visualization of the data structures
    
    * Removed unused backend code
    
    * Changes to be removed
    
    * changes to be reverted
    
    * Revert "Initial commit"
    
    This reverts commit 8c96fc2f8d1169fc4b10d56545ad642778b22203.
    
    * First commit this contains the raw frontend
    
    * Changed project name to fronend from v
    
    * Revert "Initial commit"
    
    This reverts commit 8c96fc2f8d1169fc4b10d56545ad642778b22203.
    
    * Initial commit
    
    * Delete public directory
    
    * Delete src directory
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Delete .gitignore
    
    * Changed from .Next to react
    
    * created a react type scrip app
    
    * Created header component
    
    * Created the Home page, created routes to other pages
    
    * Chaged site meeta data and added headers to each page
    
    * added set, list, dict and tree data structures and updated playground page
    
    * Updated datastructures
    
    * changes
    
    * Update package.json to add axios
    
    * removed gitignore packages
    
    * added ui components
    
    * linked cards to pages
    
    * fixed routing issue
    
    * made animatin changes
    
    * added the functionality to start the server when the first page is loaded
    
    * addded the correct api url
    
    * fixed issue website now calls the request when page is initially loaded
    
    ---------
    
    Co-authored-by: Evans Asuboah <easuboah@stetson.edu>
    Co-authored-by: RichieNibe <RICHARDNIBE2@GMAIL.COM>
    Co-authored-by: RichieNibe <88266053+RichieNibe@users.noreply.github.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,25 @@
+"use client";
 import Header from "./components/Header/Header";
 import Card from "./components/Card/Card";
 import Link from 'next/link';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { start } from "repl";
 
 export default function Home() {
-  return (
+  useEffect(() => {
+    const startServer = async () => {
+      try {
+        const response = await axios.get('https://backend-visualize-my-code.onrender.com');
+        console.log('API response:', response.data);
+      } catch (error) {
+        console.error('Error starting server:', error);
+      }
+    };
 
+    startServer();
+  }, []);
+  return (
     <div >
 
       <Header />
